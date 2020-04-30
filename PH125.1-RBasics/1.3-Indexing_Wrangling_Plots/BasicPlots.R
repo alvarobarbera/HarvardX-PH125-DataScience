8fe4946b2e4d24d31487dd1e2492ec3fe39f851f
alvarobarbera <alvarobarbera@gmail.com>
2020-05-01 01:36:02 +1000

    R basics

--- PH125.1-RBasics/1.3-Indexing_Wrangling_Plots/BasicPlots.R
+++ PH125.1-RBasics/1.3-Indexing_Wrangling_Plots/BasicPlots.R
@@ -7,3 +7,19 @@ data("murders")
 x <- murders$population / 10^6
 y <- murders$total
 plot(x, y)
+
+
+# hist
+
+x <- with(murders, total/population*10^5)
+hist(x)
+
+# boxplot
+boxplot(rate~region, data = murders)
+
+
+# The image function displays the values in a matrix using color. Here is a quick example:
+  
+x <- matrix(1:120, 12, 10)
+image(x)
+
